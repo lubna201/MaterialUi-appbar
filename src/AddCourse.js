a781a9071987722875968000db12e72a7edd0930
lubna201 <lubnasazida@gmail.com>
2021-06-22 10:18:48 +0600

    k

--- src/AddCourse.js
+++ src/AddCourse.js
@@ -9,7 +9,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
+// import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
