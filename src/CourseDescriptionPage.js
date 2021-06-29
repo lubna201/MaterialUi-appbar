274668aa676499765f5e76528d94bb5ad4af104d
lubna201 <lubnasazida@gmail.com>
2021-06-29 18:54:03 +0600

    cd

--- src/CourseDescriptionPage.js
+++ src/CourseDescriptionPage.js
@@ -7,7 +7,7 @@ import Button from '@material-ui/core/Button';
 import pic from './pic.jpg';
 import Rating from '@material-ui/lab/Rating';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import ClassIcon from '@material-ui/icons/Class';
 import ScheduleIcon from '@material-ui/icons/Schedule';
